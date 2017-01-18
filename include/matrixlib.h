787887ff2c5d7268a2a85e45273683b952dcac72
kushal <ksh.sah@gmail.com>
2017-01-18 18:05:33 -0500

    finished all matrixlib class definitions- matrix product and transpose

--- include/matrixlib.h
+++ include/matrixlib.h
@@ -6,43 +6,46 @@
 #include<vector>
 #include<stdexcept>
 
+#define RANGE_CHECK
 
 template <class T>
 
 class MyMatrix {
-    
-    std::vector<std::vector<T> > mat_;
-    unsigned rows_;
-    unsigned cols_;
-    public:
 
-    //constructor
-    
-    MyMatrix(unsigned rows, unsigned cols, const T& initial=0);
-    MyMatrix(const MyMatrix<T>& rhs);
-    virtual ~MyMatrix();
+  std::vector<std::vector<T> > mat_;
+  unsigned rows_;
+  unsigned cols_;
 
-    // Operator overloading, for "standard" mathematical matrix operations                                                                                                                                                          
-    MyMatrix<T>& operator=(const MyMatrix<T>& rhs);
 
-    // Matrix mathematical operations                                                                                                                                                                                               
-    MyMatrix<T> operator+(const MyMatrix<T>& rhs);
-    MyMatrix<T>& operator+=(const MyMatrix<T>& rhs);
-    MyMatrix<T> operator-(const MyMatrix<T>& rhs) const;
-    MyMatrix<T>& operator-=(const MyMatrix<T>& rhs);
-    MyMatrix<T> operator*(const MyMatrix<T>& rhs);
-    MyMatrix<T>& operator*=(const MyMatrix<T>& rhs);
-    MyMatrix<T> operator~(const 
+ protected:
+  void range_check(unsigned i, unsigned j) const;
 
-    MyMatrix<T> transpose() const;
+ public:
 
-    // Access the individual elements                                                                                                                                                                                               
-    T& operator()(const unsigned& row, const unsigned& col);
-    const T& operator()(const unsigned& row, const unsigned& col) const;
+  //constructor
 
-    // Access the row and column sizes                                                                                                                                                                                              
-    unsigned get_rows() const;
-    unsigned get_cols() const;
+  MyMatrix(unsigned rows, unsigned cols, const T& initial);
+  MyMatrix(const MyMatrix<T>& rhs);
+  virtual ~MyMatrix();
+
+  // Operator overloading 
+  MyMatrix<T>& operator=(const MyMatrix<T>& rhs);
+
+  // Matrix Multiplication                                                                                                                                                                                               
+  MyMatrix<T> operator*(const MyMatrix<T>& rhs) const;
+
+
+  // Matrix Transpose
+  MyMatrix<T> transpose() const ;
+  MyMatrix<T> operator~() const ;
+
+  // Access the individual elements                                                                                                                                                                                               
+  T& operator()(const unsigned& row, const unsigned& col);
+  const T& operator()(const unsigned& row, const unsigned& col) const ;
+
+  // Access the row and column sizes                                                                                                                                                                                              
+  unsigned get_rows() const;
+  unsigned get_cols() const;
 
 };
 
--- src/matrixlib.cpp
+++ src/matrixlib.cpp
@@ -3,18 +3,19 @@
 template<class T>
 MyMatrix<T>::MyMatrix(unsigned rows, unsigned cols, const T& initial):rows_(rows),cols_(cols) {
 
-    if( rows_ == 0 || cols_ == 0 )
-        throw std::range_error("Degenerate Matrix creation!Aborted");
+  if( rows_ == 0 || cols_ == 0 )
+    throw std::range_error("Degenerate Matrix creation!Aborted");
 
-    mat(rows_, std::vector<T>(cols_,initial); 
-//    mat.resize(rows_);
-//    for (unsigned i=0; i<mat.size(); i++) {
-//        mat[i].resize(cols, initial);
-//    }
+  mat(rows_, std::vector<T>(cols_,initial)); 
+      //    mat.resize(rows_);
+      //    for (unsigned i=0; i<mat.size(); i++) {
+      //        mat[i].resize(cols, initial);
+      //    }
 
-}
+      }
 
-//copy constructor
+      //copy constructor
+
 
 template<class T>
 MyMatrix<T>::MyMatrix(const MyMatrix<T>& rhs):rows_(rhs.rows_),cols_(rhs.cols_), mat_(rhs.mat_){}
@@ -24,54 +25,46 @@ template<class T>
 MyMatrix<T>::~MyMatrix() {}
 
 template<class T>
-MyMatrix<T>& MyMatrix<T>::operator=( const matrix<T>& rhs )
+MyMatrix<T>& MyMatrix<T>::operator=( const MyMatrix<T>& rhs )
 {
-    if(rhs.rows_ != rows_ && rhs.cols_ != cols_ )
-        throw std::domain_error("matrix op= not of same order");
-    if (&rhs == this){
-        return *this;
-    }
-    for(unsigned i=0;i<rows_*cols_;i++)
-        this->elements_[i] = rhs.elements_[i];
+  if(rhs.rows_ != rows_ && rhs.cols_ != cols_ )
+    throw std::domain_error("matrix op= not of same order");
+  if (&rhs == this){
     return *this;
-}
-
-template<class T>
-MyMatrix<T> MyMatrix<T>::operator+(const MyMatrix<T>& rhs) const {
-    MyMatrix result(rows, cols, 0.0);
-
-    for (unsigned i=0; i<rows; i++) {
-        for (unsigned j=0; j<cols; j++) {
-            result(i,j) = this->mat[i][j] + rhs(i,j);
-        }
-    }
-
-    return result;
-}
-
-
-MyMatrix<T> operator*( const Myatrix<T>& rhs ) const{
-
-
-
-
+  }
+  for(unsigned i=0;i<rows_*cols_;i++)
+    this->elements_[i] = rhs.elements_[i];
+  return *this;
 }
 
 
 template<class T>
-MyMatrix<T> operator~() const {
-         return this->transpose();
+MyMatrix<T> MyMatrix<T>::operator*( const MyMatrix<T>& rhs ) const{
+  if(cols_ != rhs.rows_ )
+    throw std::domain_error("Aborted! Matrix multiplication dimension mismatch! number of columns of the first matrix should be equal to number of rows of the second matrix");
+  
+  MyMatrix<T> temp(rows_, cols_, T(0.0));
+
+  for (unsigned i=0; i<rows_; i++) {
+    for (unsigned j=0; j<rhs.cols_; j++) {
+      for (unsigned k=0; k<cols_; k++) {
+        temp.mat[i][j] += this->mat_[i][k] * rhs(k,j);
       }
+    }
+  }
+
+  return temp;
+}
 
 
 // Calculate a transpose of this matrix                                                                                                                                       
 template<class T>
-MyMatrix<T> MyMatrix<T>::transpose() {
-  MyMatrix temp(rows_, cols_, 0.0);
+MyMatrix<T> MyMatrix<T>::transpose()const{
+  MyMatrix<T> temp(rows_, cols_, T(0.0));
 
   for (unsigned i=0; i<rows_; i++) {
     for (unsigned j=0; j<cols_; j++) {
-      temp(i,j) = this->mat[j][i];
+      temp(i,j) = this->mat_[j][i];
     }
   }
 
@@ -80,26 +73,34 @@ MyMatrix<T> MyMatrix<T>::transpose() {
 
 
 template<class T>
+MyMatrix<T> MyMatrix<T>::operator~()const{
+  return this->transpose();
+}
+
+
+template<class T>
 void MyMatrix<T>::range_check( unsigned i, unsigned j ) const
 {
-   if( i<0 ||  rows_ <= i )
-      throw std::range_error("matrix access row out of range");
-   if( j < 0 || cols_ <= j  )
-      throw std::range_error("matrix access col out of range");
+  if( i<0 ||  rows_ <= i )
+    throw std::range_error("matrix access row out of range");
+  if( j < 0 || cols_ <= j  )
+    throw std::range_error("matrix access col out of range");
 }
 
-
-T& operator()( const unsigned i, const unsigned j ) {
+template<class T>
+T& MyMatrix<T>::operator()( const unsigned& i, const unsigned& j ) {
 #ifdef RANGE_CHECK
-    range_check(i,j);
+  range_check(i,j);
 #endif
-    return mat_[i][j];
+  return mat_[i][j];
 }
-const T& operator()( const unsigned i, const unsigned j ) {
+
+template<class T>
+const T& MyMatrix<T>::operator()( const unsigned& i, const unsigned& j ) const{
 #ifdef RANGE_CHECK
-    range_check(i,j);
+  range_check(i,j);
 #endif
-    return mat_[i][j];
+  return mat_[i][j];
 }
 
 // Get the number of rows of the matrix                                                                                                                                       
